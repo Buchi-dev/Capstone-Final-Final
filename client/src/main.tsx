aadfdbb776cd778760f42f5e6ba64f3838e1cef2
Buchi-dev <hed-tjyuzon@smu.edu.ph>
2025-10-19 21:02:28 +0800

    feat: Update report generation functionality and remove deprecated Reports component

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,7 +7,7 @@ import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider themeMode="light">
+    <ThemeProvider themeMode="dark">
       <App />
     </ThemeProvider>
   </StrictMode>,
